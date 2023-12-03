61cd775d7593b5a55cf8d0d81daed8d96bc73930
Lisa-Maria Förster <Lisamari9500@gmail.com>
2023-12-03 18:16:09 +0100

    add js.

---
+++ js/script.js
@@ -0,0 +1,88 @@
+'use strct'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let quantityInput = document.getElementById("quantity");
+// quantityInput.addEventListener("input", updateOutput);
+
+// function updateOutput() {
+//   const quantity = quantityInput.value;;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // innerHTML
+
+
+// // body
+// // <form id="shop"></form>
+
+
+// // <!-- <script src="js/script.js"></script> -->
+// // <script>
+
+// //     // Select the element with id "shop"
+// //     let formElement = document.querySelector('#shop');
+
+// //     // Set the inner HTML of the selected element
+// //     formElement.innerHTML = '<p>hello world</p>';
+
+// // </script>
